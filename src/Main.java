9db67956352cd0e8d8e4f8d6641efc53aa37a79e
Aisha Saleem <as2634@kent.ac.uk>
2022-03-29 10:56:44 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,13 @@ public class Main {
         // START OF YOUR CODE
 
         // Task 1
+        // Identify the longest line in the list lyrics and print out the longest line and its length in the terminal.
         System.out.println("Task1");
         lyrics.stream().max(Comparator.comparing(String::length)).ifPresent(t -> System.out.println(t.length() + ":" + t));
         System.out.println();
+
         // Task 2
+        // Collect all words in lyrics and store them in a local variable named words of type List<String>
         System.out.println("Task2");
         List<String> words = lyrics.stream().flatMap(w -> Arrays.stream(w.toLowerCase().replace(",", "").split(" ")))
                 .collect(Collectors.toList());
@@ -29,11 +32,13 @@ public class Main {
 
 
         // Task 3
+        //Print out a list of distinct words with 6 characters in an alphabetical order
         System.out.println("Task3");
         words.stream().distinct().filter(t -> t.length() == 6).sorted().forEach(System.out::println);
         System.out.println();
 
         // Task 4
+        // Calculate the frequency of each unique word in words and then print out the words and their frequencies of all the words that have at    least 4 characters and occur more than 5 times. The output should be ordered by frequency
         System.out.println("Task4");
         words.stream().filter(t -> t.length() >= 4).collect(groupingBy(t -> t, counting())).entrySet()
                 .stream().filter(t -> t.getValue() >= 5).sorted(Comparator.comparing(Map.Entry::getValue))
